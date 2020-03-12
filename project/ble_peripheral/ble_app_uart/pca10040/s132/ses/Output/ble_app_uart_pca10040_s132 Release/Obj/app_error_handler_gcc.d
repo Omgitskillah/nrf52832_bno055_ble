c7b266f8f3381ba6945ce3ef76b248823a5ca04e
Clarence Alucho <clarence.alucho@gmail.com>
2020-03-12 16:41:27 +0300

    Updated repo with use instructions

---
+++ project/ble_peripheral/ble_app_uart/pca10040/s132/ses/Output/ble_app_uart_pca10040_s132 Release/Obj/app_error_handler_gcc.d
@@ -0,0 +1,32 @@
+Output/ble_app_uart_pca10040_s132\ Release/Obj/app_error_handler_gcc.o: \
+ C:\git\nrf52832_bno055_ble\components\libraries\util\app_error_handler_gcc.c \
+ C:\git\nrf52832_bno055_ble\components\libraries\util\sdk_common.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.42a/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.42a/include/stdbool.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.42a/include/string.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.42a/include/__crossworks.h \
+ ../config/sdk_config.h \
+ C:\git\nrf52832_bno055_ble\components\libraries\util\nordic_common.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ C:\git\nrf52832_bno055_ble\components\libraries\util\sdk_os.h \
+ C:\git\nrf52832_bno055_ble\components\libraries\util\sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ C:\git\nrf52832_bno055_ble\components\libraries\util\app_util.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.42a/include/stddef.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ C:\git\nrf52832_bno055_ble\components\libraries\util\sdk_macros.h \
+ C:\git\nrf52832_bno055_ble\components\libraries\util\nrf_assert.h \
+ C:\git\nrf52832_bno055_ble\components\libraries\util\app_error.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.42a/include/stdio.h \
+ C:\git\nrf52832_bno055_ble\components\libraries\util\app_error_weak.h
---
+++ project/ble_peripheral/ble_app_uart/pca10040/s132/ses/Output/ble_app_uart_pca10040_s132 Release/Obj/system_nrf52.d
@@ -0,0 +1,16 @@
+Output/ble_app_uart_pca10040_s132\ Release/Obj/system_nrf52.o: \
+ C:\git\nrf52832_bno055_ble\modules\nrfx\mdk\system_nrf52.c \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.42a/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.42a/include/stdbool.h \
+ C:\git\nrf52832_bno055_ble\modules\nrfx\mdk\nrf.h \
+ C:\git\nrf52832_bno055_ble\modules\nrfx\mdk\nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ C:\git\nrf52832_bno055_ble\modules\nrfx\mdk\system_nrf52.h \
+ C:\git\nrf52832_bno055_ble\modules\nrfx\mdk\nrf52_bitfields.h \
+ C:\git\nrf52832_bno055_ble\modules\nrfx\mdk\nrf51_to_nrf52.h \
+ C:\git\nrf52832_bno055_ble\modules\nrfx\mdk\nrf52_name_change.h \
+ C:\git\nrf52832_bno055_ble\modules\nrfx\mdk\compiler_abstraction.h
